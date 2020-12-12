c17b49e619d0d18e2739de897a1e939372b6a144
Arindam <msaha6368@gmail.com>
2020-12-12 20:35:49 +0530

    sign up and login button functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+// DOM elements 
+const mainPage = document.querySelector('.main-page');
+const loginPage = document.querySelector('.login-page');
+const middleContent = document.querySelector('.middle-content');
+
+/*****************************************************/
+/*****************************************************/
+
+
+// Main page
+const goToLoginPage = () => {
+    mainPage.style.display = 'none';
+    loginPage.style.display = 'grid';
+}
+
+middleContent.addEventListener('click', e => {
+    if (e.target.classList[1] === 'main-btn') {
+        goToLoginPage();
+    }
+})
